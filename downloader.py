e7600e0d56e9af8bb9f06266f575c4df7a3b127e
om1ji <om1ji.music@gmail.com>
2021-07-07 15:51:48 +0300

    fixed bugs, --no-part

--- downloader.py
+++ downloader.py
@@ -4,16 +4,16 @@ import json
 import sqlite3
 import time
 import requests
-from requests.api import request
 
 import telebot
 
 from regex import *
-from _logging import *
+from _logging import _log
 
-DIRECTION = r'/home/bot/HATE/Files/'
+DIRECTION = r'/home/bot/HATE/'
+RESULT_DIR = DIRECTION + 'tmp/'
 QUEUE_DIR = DIRECTION + 'queue.db'
-LOGFILE = DIRECTION + "dowloader-log.txt"
+LOGFILE = DIRECTION + "downloader-log.txt"
 TOKEN = '1591601193:AAHWLplYpkAPwbwq7c-0A51169BQpf9N04s'
 CHAT_ID = -1001389676477
 TMP_CHAT_ID = -1001170446896
@@ -21,29 +21,31 @@ BOT = telebot.TeleBot(TOKEN)
 
 
 def download_from_queue(QUEUE_DIR):
+    print(QUEUE_DIR)
     _con = sqlite3.connect(QUEUE_DIR)
     cur = _con.cursor() 
     while True:
         os.chdir(DIRECTION)
         cur.execute("SELECT * FROM queue")
-        current_link = cur.fetchone()
+        current_link = cur.fetchone()[0]
         if current_link:
-            _log(LOGFILE, "Fetched link: " + current_link + ", running downloading....")
+            _log(LOGFILE, "Fetched link: " + str(current_link) + ", running downloading....")
             # Run downloading
-            os.system(f"""youtube-dl -x https://www.youtube.com/watch?v={current_link} 
-                        --audio-format mp3 --audio-quality 0 
-                        --write-description -o "./%(id)s/%(title)s-%(id)s.%(ext)s" 
+            os.system(f"""youtube-dl -x {current_link} \
+                        --audio-format mp3 --audio-quality 0 --no-part \
+                        --write-description -o "./tmp/%(id)s/%(title)s-%(id)s.%(ext)s" \
                         --write-thumbnail""")
             _log(LOGFILE, "Downloading finished!", 1)
-            folder = DIRECTION + current_link + '/' #Путь до папки
+            folder = RESULT_DIR + current_link[-11:] + '/' #Путь до папки
             os.chdir(folder)
+            _log(LOGFILE, "Fole", 1)
             basename = os.path.splitext(os.listdir()[0])[0]
 
             track_descr = folder + basename + '.description' #Путь до файла .description
             read_track_descr = open(track_descr).read()
             _log(LOGFILE, f"Folder: {folder}, path to .description file: {track_descr}", 1)
             single_file = {'document': open(folder + basename + '.mp3', 'rb')}
-            thumbnail = {'document': open(folder + basename + '.jpg', 'rb')}
+            thumbnail = {'document': open(folder + basename + '.webp', 'rb')}
 
             _log(LOGFILE, "Sending to temp channel...", 1)
             the_file = requests.post(f"https://api.telegram.org/bot{TOKEN}/sendDocument?chat_id={TMP_CHAT_ID}", files=single_file)
@@ -80,7 +82,8 @@ def download_from_queue(QUEUE_DIR):
             _con.commit()
             _log(LOGFILE, "Row deleted from the db", 1)
             _log(LOGFILE, "=CYCLE ENDED=" , 1)
-        time.sleep(15)
+        else:
+            time.sleep(15)
 
 if __name__ == '__main__':
     download_from_queue(QUEUE_DIR)
