723c3b39eb9313557c2526ca22d874d8da92d35e
artemetra <artemlukin15@gmail.com>
2021-07-26 14:33:16 +0300

    rewritten db_retry_until_unlocked, fixed bugs

--- utils.py
+++ utils.py
@@ -15,32 +15,37 @@ def _log(file, text, tabbing = 0):
     with open(file, 'a', encoding='utf-8') as f:
         f.write("[{}]{} {}\n".format(str(datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f"))[:-3], "\t"*tabbing, text))
 
-def db_retry_until_unlocked(logfile, cur, cmd: str, time = 2, max_retries = 10):
+def db_retry_until_unlocked(logfile, directory: str, cmd: str, sleep_time = 2, max_retries = 10):
     """
     Handles the 'database is locked' exception
     and tries to execute the function until the db
-    gets unlocked.
+    gets unlocked, and closes the connection afterwards.
     :param file: logfile
-    :param cur: sqlite cursor
+    :param directory: directory of the db
     :param cmd: sqlite command 
     :param time: sleep time between attempts in seconds (default = 2)
     :param max_retries: amounts of attempts allowed until breaking (default = 10)
     """
     _flag = False
     retry_count = 0
+    con = sqlite3.connect(directory)
+    cur = con.cursor()
     while not _flag:
         if retry_count < max_retries:
             try:
                 cur.execute(cmd)
+                con.commit()
                 _flag = True
             except sqlite3.OperationalError:
-                _log(logfile, f"[{retry_count}]Database is locked, reattempting again in {time} seconds...", 2)
+                _log(logfile, f"[{retry_count}]Database is locked, reattempting again in {sleep_time} seconds...", 2)
                 retry_count += 1
-                sleep(time)
+                sleep(sleep_time)
         else:
             _log(logfile, f"======Reached max retries ({max_retries}), breaking")
             raise ValueError("Reached max retries")
-
+    fetched = cur.fetchall()
+    con.close()
+    return fetched
 
 if __name__ == '__main__':
     """Tests"""
