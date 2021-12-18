aa311c0053b37e76606b1350c1eda7ded5001757
artemetra <artemlukin15@gmail.com>
2021-12-18 16:49:03 +0200

    Revert "Revert "rewriting stuff""
    This reverts commit fe0cb927cdd5f4557a99e44ea979118a3b718d7e.

--- server_linux.py
+++ server_linux.py
@@ -1,18 +1,19 @@
 import re
-import sqlite3
 import yaml
 
 from flask import Flask, request
+app = Flask(__name__)
 
 from regex import *
-from utils import _log, db_retry_until_unlocked
-
-app = Flask(__name__)
+from utils import Log, db_retry_until_unlocked
+from ORM import SQL 
 
 DIRECTION = r'/home/bot/HATE/'
 CONFIG = yaml.safe_load(open(DIRECTION + 'config.yml', 'r'))
 LOGFILE = DIRECTION + CONFIG['server_linux_logfile']
 QUEUE_DIR = DIRECTION + CONFIG['queue_name']
+db = SQL(QUEUE_DIR)
+l = Log(LOGFILE)
 #================================================================
 
 def extract_link(raw: bytes) -> str:
@@ -34,19 +35,14 @@ def webhook():
             return request.args.get('hub.challenge', '')
 
     elif request.method == 'POST':
-        _log(LOGFILE, "Incoming webhook with following data: " + str(request.data))
+        l.log(LOGFILE, f"Incoming webhook with following data: {request.data}")
         link = extract_link(request.data)
 
-        db_retry_until_unlocked(LOGFILE, QUEUE_DIR, f'''
-                                                     INSERT INTO queue (link)
-                                                     VALUES (\'{link}\');
-                                                     ''')
-        _log(LOGFILE, f"Link \"{link}\" from {extract_link} inserted!")
+        db.push_to_queue(link)
+        
+        l.log(LOGFILE, f"Link \"{link}\" from {extract_link} inserted!")
         return '200'
 
 if __name__=='__main__':
-    db_retry_until_unlocked(LOGFILE, QUEUE_DIR, '''
-                                                CREATE TABLE IF NOT EXISTS queue
-                                                (link text UNIQUE);
-                                                ''')
+    db.create_table()
     app.run()
